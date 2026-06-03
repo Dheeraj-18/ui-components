491aa51ed696673b1f16b45b60614f1b590e1d99
think2code <dheerajsaraswat03@gmail.com>
2026-06-03 10:09:39 +0530

    dialog:implement fully accessible with all req dialog component

--- Modal-Dialog/REQ.md
+++ Modal-Dialog/REQ.md
@@ -8,3 +8,17 @@
 - It should be fully Accessible
 
 ## Accessibility: How to 
+
+1. It should cover all the screen, nothing should be intractable behind dialog, No Scroll. No Focus.
+
+2. Focus Capturing: Via Tab or Sift + Tab focus should not leave the dialog. Until the dialog is closed.
+
+3. When we Open Dialog, first focusable element should get focused.
+
+4. When we close the dialog, the focus set back to the triggered element, and if triggered element is not there, focus should be added on the relevant element.
+
+5. aria-labelled by should there
+
+6. if there is description in the dialog, add id to it, and use aria-
+
+
--- Modal-Dialog/src/Components/Dialog.jsx
+++ Modal-Dialog/src/Components/Dialog.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
 import './style.css'
 
 function Dialog({ onClose, children }) {
-  return (
-    <div className="dialog">
-      <div className="dialog-backdrop"></div>
-      <div className="dialog-content">
-        {!!onClose && <button onClick={onClose} className="dialog-close">&times;</button>}
+  const contentRef = useRef()
+  const backdropRef = useRef()
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyUp)
+    blockOutsideAccess()
+
+    return () => {
+      unBlockOutsideAccess()
+      document.removeEventListener('keydown', handleKeyUp)
+    }
+  }, [])
+
+  function blockOutsideAccess() {
+    const body = document.getElementsByTagName('body')[0]
+
+    ;[...body.children].forEach((el) => {
+      if (!el.getAttribute('data-dialog')) {
+        el.setAttribute('aria-hidden', true)
+        el.setAttribute('inert', true)
+      }
+    })
+  }
+  function unBlockOutsideAccess() {
+    const body = document.getElementsByTagName('body')[0]
+
+    ;[...body.children].forEach((el) => {
+      if (!el.getAttribute('data-dialog')) {
+        el.removeAttribute('aria-hidden')
+        el.removeAttribute('inert')
+      }
+    })
+  }
+
+  function handleKeyUp(e) {
+    if (e.key === 'Escape') {
+      handleClose()
+    }
+  }
+
+  const handleClose = () => {
+    contentRef.current.classList.add('hide-dialog')
+    backdropRef.current.classList.add('hide-dialog')
+
+    contentRef.current.addEventListener('animationend', handleAnimationEnd, {
+      once: true,
+    })
+  }
+
+  function handleAnimationEnd() {
+    onClose()
+  }
+
+  return createPortal(
+    <div data-dialog="true" className="dialog">
+      <div
+        onClick={handleClose}
+        ref={backdropRef}
+        className="dialog-backdrop"
+      ></div>
+      <div ref={contentRef} className="dialog-content">
+        {!!onClose && (
+          <button onClick={handleClose} className="dialog-close">
+            &times;
+          </button>
+        )}
         {children}
       </div>
-    </div>
+    </div>,
+    document.getElementsByTagName('body')[0],
   )
 }
 
