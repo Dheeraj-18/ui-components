2655fab28ce241f20316ff532eab4bb004fee21d
think2code <dheerajsaraswat03@gmail.com>
2026-06-04 11:03:03 +0530

    (tab-compo):implement fully accessible and reusable component

--- Tab-list-compo/src/App.jsx
+++ Tab-list-compo/src/App.jsx
@@ -6,12 +6,46 @@ import './App.css'
 import Tablist from './Components/Tablist'
 
 function App() {
+  const tabList = [
+    {
+      id: 'a',
+      label: 'component A',
+      Component: ComponentA,
+    },
+    {
+      id: 'b',
+      label: 'component B',
+      Component: ComponentB,
+    },
+    {
+      id: 'c',
+      label: 'component C',
+      Component: ComponentC,
+    },
+    {
+      id: 'd',
+      label: 'component D',
+      Component: ComponentD,
+    },
+  ]
 
+  function ComponentA() {
+    return <h1>Component A</h1>
+  }
+  function ComponentB() {
+    return <h1>Component B</h1>
+  }
+  function ComponentC() {
+    return <h1>Component C</h1>
+  }
+  function ComponentD() {
+    return <h1>Component D</h1>
+  }
 
   return (
-      <div className='app'>
-        <Tablist/>
-      </div>
+    <div className="app">
+      <Tablist tabs={tabList} />
+    </div>
   )
 }
 
--- Tab-list-compo/src/Components/Tablist.jsx
+++ Tab-list-compo/src/Components/Tablist.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
 
-function Tablist() {
+function Tablist({ tabs }) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const handleTabChange = (index) => {
+    setSelectedIndex(index)
+  }
+
+  const Component = tabs[selectedIndex].Component
+
   return (
-    <div>Tablist</div>
+    <div className="tab-list" role="tab-list">
+      <div className="tab-container">
+        {tabs.map((tb, index) => {
+          return (
+            <button
+              onClick={() => handleTabChange(index)}
+              key={tb.id}
+              data-selected={index === selectedIndex}
+              aria-selected={index === selectedIndex}
+              role="tabs"
+            >
+              {tb.label}
+            </button>
+          )
+        })}
+      </div>
+      <div role="tabpanel">
+        <Component />
+      </div>
+    </div>
   )
 }
 
