c8e14d3667ddb6b0b65be59da91c26af4e1e5c87
think2code <dheerajsaraswat03@gmail.com>
2026-06-04 09:59:47 +0530

    (tab-comp):initial setup with req for tab component

---
+++ Tab-list-compo/REQ.md
@@ -0,0 +1,5 @@
+# Tab -Component 
+
+- Show tablist and tab component based on Selected Tab
+- Check edge cases for Optimization ( Open Ended
+)
---
+++ Tab-list-compo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Tablist from './Components/Tablist'
+
+function App() {
+
+
+  return (
+      <div className='app'>
+        <Tablist/>
+      </div>
+  )
+}
+
+export default App
