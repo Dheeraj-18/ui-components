b9a8f7d14a49614895d47b11ef7607d0d9872e86
think2code <dheerajsaraswat03@gmail.com>
2026-06-06 06:46:24 +0530

    (search-bar):initial setup for search bar compo with req

---
+++ Autocomplete-searchBar/REQ.md
@@ -0,0 +1,5 @@
+# Autocomplete Search Bar
+- Implement the search bar with Which take data on query params 
+- Improve their performance by implementing the Debouncing 
+- Also implement the cache 
+- with proper styling and event on that component
---
+++ Autocomplete-searchBar/src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import SearchBar from './components/searchBar'
+
+function App() {
+  return (
+    <div className="app">
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App
