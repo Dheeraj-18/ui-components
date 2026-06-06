1eeb5c8a0341b34b38ba59531dd4acc763d12d31
think2code <dheerajsaraswat03@gmail.com>
2026-06-06 11:47:25 +0530

    (auto-search):implement the cache with state to improve performance

--- Autocomplete-search/src/components/SearchBar.jsx
+++ Autocomplete-search/src/components/SearchBar.jsx
@@ -5,11 +5,17 @@ function SearchBar() {
   const [input, setInput] = useState('')
   const [result, setResult] = useState([])
   const [show, setShow] = useState(false)
+  const [cache, setCache] = useState({})
 
   const fetchData = async () => {
+    if (cache[input]) {
+      setResult(cache[input])
+      return
+    }
     const data = await fetch(`https://dummyjson.com/recipes/search?q=` + input)
     const json = await data.json()
     setResult(json?.recipes)
+    setCache((prev) => ({ ...prev, [input]: json.recipes }))
   }
 
   useEffect(() => {
